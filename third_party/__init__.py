281c1c07d0e0d72a014583f778f3233822850b9e
Evan Hernandez <evanfredhernandez@gmail.com>
2021-03-30 16:38:02 -0400

    Add netdissect.

--- third_party/__init__.py
+++ third_party/__init__.py
@@ -1,1 +1,9 @@
-"""Third-party libraries that have been modified for this project."""
+"""Code written by other people.
+
+This code is (mostly) unmodified, but must be stored with the repository
+because it isn't available via pip. It's usually research code.
+
+Someone in the back asks: "Why not use git submodules?"
+I answer: because they are a huge pain. Also, we often need to make
+small adjustments to the imports so that the code runs.
+"""
