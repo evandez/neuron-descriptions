4f5c363ef5b954dfef439e414b016810cb49e6f3
Evan Hernandez <evanfredhernandez@gmail.com>
2021-04-13 15:59:41 -0400

    Add new, unchanged netdissect.

--- vocabulary/dissection.py
+++ vocabulary/dissection.py
@@ -1,12 +1,14 @@
 """Functions for dissecting individual units in vision models."""
 import pathlib
 import shutil
-from typing import Any, Callable, Optional, Tuple, Union
+from typing import Any, Callable, Iterator, Optional, Sequence, Tuple, Union
 
-from third_party.netdissect_old import imgsave, imgviz, nethook, pbar, tally
+from third_party.netdissect import (imgsave, imgviz, nethook, pbar,
+                                    runningstats, tally)
 
 import numpy
 import torch
+from PIL import Image
 from torch import nn
 from torch.utils import data
 
@@ -17,6 +19,86 @@ ComputeTopKAndQuantileFn = Callable[..., TensorPair]
 ComputeActivationsFn = Callable[..., Union[torch.Tensor, TensorPair]]
 
 PathLike = Union[str, pathlib.Path]
+
+UnitRank = Tuple[int, int]
+IndividualMaskedImages = Sequence[Sequence[Image.Image]]
+
+
+class ImageVisualizer(imgviz.ImageVisualizer):
+    """A NetDissect ImageVisualizer with additional utility functions."""
+
+    def image_and_mask_grid_for_topk(
+            self, compute: ComputeActivationsFn, dataset: data.Dataset,
+            topk: runningstats.RunningTopK,
+            **kwargs: Any) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Return top masks, images, and masked images.
+
+        You can think of this method as a counterpart to the NetDissect
+        `ImageVisualizer.masked_image_grid_for_topk` method. The only real
+        difference is that, in addition to returning the masked images,
+        it also returns the binary masks and the original unmasked images.
+
+        The **kwargs are forwarded to `tally.gather_topk`.
+
+        Args:
+            compute (ComputeActivationsFn): Compute function. Should return
+                activations given a batch from the dataset.
+            dataset (data.Dataset): The dataset to compute activations on.
+                See `tally.gather_topk`.
+            topk (runningstats.RunningTopK): Top-k results.
+                See `tally.gather_topk`.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: Image masks
+                of shape (units, k, 1, height, width), unmasked images
+                of shape (units, k, 3, height, width), masked images
+                of shape (units, k, 3, height, width), all byte tensors.
+
+        """
+
+        def compute_viz(
+                gather: Sequence[Sequence[UnitRank]],
+                *batch: Any) -> Iterator[Tuple[UnitRank, torch.Tensor]]:
+            activations = compute(*batch)
+            if isinstance(activations, tuple):
+                activations, images = activations
+            else:
+                images, = batch
+            for ranks, acts, image in zip(gather, activations, images):
+                for unit, rank in ranks:
+                    image = self.pytorch_image(image)
+                    mask = self.pytorch_mask(acts, unit)
+                    masked = self.pytorch_masked_image(image,
+                                                       mask=mask,
+                                                       outside_bright=.25,
+                                                       thickness=0)
+                    result = torch.cat([
+                        component.clamp(0, 255).byte().cpu()
+                        for component in (masked, image, mask[None])
+                    ])
+                    yield ((unit, rank), result)
+
+        gt = tally.gather_topk(compute_viz, dataset, topk, **kwargs).result()
+        masked, images, mask = gt[:, :, :3], gt[:, :, 3:6], gt[:, :, 6:]
+        return masked, images, mask
+
+    def individual_masked_images_for_topk(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Tuple[Sequence[Sequence[Image.Image]], torch.Tensor, torch.Tensor]:
+        """Return individual masked PIL images and separate masks/images.
+
+        You can think of this method as a counterpart to the NetDissect
+        `ImageVisualizer.individual_masked_images_for_topk` method. The only
+        difference is it also returns the masks and images separately.
+        """
+        grids = self.image_and_mask_grid_for_topk(*args, **kwargs)
+        masked, images, masks = grids
+        individual = [
+            [Image.fromarray(mi.numpy()) for mi in mis] for mis in masked
+        ]
+        return individual, images, masks
 
 
 def dissect(compute_topk_and_quantile: ComputeTopKAndQuantileFn,
@@ -29,7 +111,7 @@ def dissect(compute_topk_and_quantile: ComputeTopKAndQuantileFn,
             num_workers: int = 30,
             results_dir: PathLike = 'dissection-results',
             tally_cache_file: Optional[PathLike] = None,
-            topk_images_cache_file: Optional[PathLike] = None,
+            masks_cache_file: Optional[PathLike] = None,
             clear_cache_files: bool = False,
             clear_results_dir: bool = False,
             display_progress: bool = True) -> None:
@@ -64,8 +146,8 @@ def dissect(compute_topk_and_quantile: ComputeTopKAndQuantileFn,
             results to. Defaults to 'dissection-results'.
         tally_cache_file (Optional[PathLike], optional): Write intermediate
             results for tally step to this file. Defaults to None.
-        topk_images_cache_file (Optional[PathLike], optional): Write
-            intermediate results for determining top-k images to this file.
+        masks_cache_file (Optional[PathLike], optional): Write intermediate
+            results for determining top-k image masks to this file.
             Defaults to None.
         clear_cache_files (bool, optional): If set, clear existing cache files
             with the same name as any of the *_cache_file arguments to this
@@ -87,7 +169,7 @@ def dissect(compute_topk_and_quantile: ComputeTopKAndQuantileFn,
 
     # Clear cache files if requested.
     if clear_cache_files:
-        for cache_file in (tally_cache_file, topk_images_cache_file):
+        for cache_file in (tally_cache_file, masks_cache_file):
             if cache_file is None:
                 continue
             cache_file = pathlib.Path(cache_file)
@@ -113,61 +195,40 @@ def dissect(compute_topk_and_quantile: ComputeTopKAndQuantileFn,
     # Now compute top images and masks for the highest-activating pixels.
     if display_progress:
         pbar.descnext('compute top images')
-
     levels = rq.quantiles(quantile).reshape(-1)
-    viz = imgviz.ImageVisualizer(image_size, source=dataset, level=levels)
-    # TODO(evandez): Migrate this function into our codebase?
-    unit_images_and_masks = viz.image_and_mask_grid_for_topk(
+    viz = ImageVisualizer(image_size, source=dataset, level=levels)
+    masked, images, masks = viz.individual_masked_images_for_topk(
         compute_activations,
         dataset,
         topk,
         k=k,
         num_workers=num_workers,
         pin_memory=True,
-        cachefile=topk_images_cache_file)
+        cachefile=masks_cache_file)
 
-    unit_images = unit_images_and_masks[:, :, :-1]
-    numpy.save(f'{results_dir}/images.npy', unit_images)
-
-    unit_masks = unit_images_and_masks[:, :, -1:]
-    numpy.save(f'{results_dir}/masks.npy', unit_masks)
-
-    # Now save the top images with the masks overlaid. A bit manual in order
-    # to avoid recomputing top images, but also pretty quick.
-    # TODO(evandez): Find a cleaner way to do the below.
-    # yapf: disable
-    unit_masked_images = imgviz.gather_tensor_to_individual_images([
-        [
-            viz.pytorch_masked_image(image,
-                                     scale=False,
-                                     mask=mask.bool().squeeze(),
-                                     outside_bright=.25,
-                                     thickness=0)
-               .permute(1, 2, 0)
-               .clamp(0, 255)
-               .byte()
-            for image, mask in zip(images, masks)
-        ]
-        for images, masks in zip(unit_images, unit_masks)
-    ])
-    # yapf: enable
-
-    pbar.descnext('saving top masked images')
-    imgsave.save_image_set(unit_masked_images,
+    # Now save the top images with the masks overlaid. We save each image
+    # individually so they can be visualized and/or shown on MTurk.
+    if display_progress:
+        pbar.descnext('saving top images')
+    numpy.save(f'{results_dir}/images.npy', images)
+    numpy.save(f'{results_dir}/masks.npy', masks)
+    imgsave.save_image_set(masked,
                            f'{results_dir}/viz/unit_%d/image_%d.png',
-                           sourcefile=topk_images_cache_file)
+                           sourcefile=masks_cache_file)
 
-    # Finally, save important metadata.
+    # Finally, save the IDs of all top images and the activation values
+    # associated with each unit and image.
     activations, ids = topk.result()
     for metadata, name, fmt in ((activations, 'activations', '%.5e'),
                                 (ids, 'ids', '%i')):
-        metadata = metadata.view(len(unit_images), k).cpu().numpy()
+        metadata = metadata.view(len(images), k).cpu().numpy()
         metadata_file = results_dir / f'{name}.csv'
         numpy.savetxt(str(metadata_file), metadata, delimiter=',', fmt=fmt)
 
+    # The lightbox lets us view all the masked images at once. Pretty handy!
     lightbox_dir = pathlib.Path(__file__).parents[1] / 'third_party'
     lightbox_file = lightbox_dir / 'lightbox.html'
-    for unit in range(len(unit_images)):
+    for unit in range(len(images)):
         unit_dir = results_dir / f'viz/unit_{unit}'
         unit_lightbox_file = unit_dir / '+lightbox.html'
         shutil.copy(lightbox_file, unit_lightbox_file)
