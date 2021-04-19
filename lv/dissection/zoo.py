0b8483224447526c05e34842f4bfaac1df20667b
Evan Hernandez <evanfredhernandez@gmail.com>
2021-04-19 18:41:43 -0400

    Assign layers in zoo config.

--- lv/dissection/zoo.py
+++ lv/dissection/zoo.py
@@ -11,6 +11,12 @@ from torchvision import datasets, transforms
 
 HOST_URL = 'http://wednesday.csail.mit.edu/dez/vocabulary/models'
 
+CONV_LAYERS_ALEXNET = ('conv1', 'conv2', 'conv3', 'conv4', 'conv5')
+CONV_LAYER_RESNET18 = ('conv1', 'layer1', 'layer2', 'layer3', 'layer4')
+CONV_LAYERS_VGG16 = ('conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1',
+                     'conv3_2', 'conv3_3', 'conv4_1', 'conv4_2', 'conv4_3',
+                     'conv5_1', 'conv5_2', 'conv5_3')
+
 
 def dissection_models() -> zoo.ModelConfigs:
     """Return configs for all models used in dissection."""
@@ -19,34 +25,40 @@ def dissection_models() -> zoo.ModelConfigs:
             'imagenet':
                 zoo.ModelConfig(models.alexnet_seq,
                                 pretrained=True,
-                                load_weights=False),
+                                load_weights=False,
+                                layers=CONV_LAYERS_ALEXNET),
             'places':
                 zoo.ModelConfig(
                     alexnet.AlexNet,
                     url=f'{HOST_URL}/alexnet/places/iter_131072_weights.pth',
-                    transform_weights=lambda weights: weights['state_dict'])
+                    transform_weights=lambda weights: weights['state_dict'],
+                    layers=CONV_LAYERS_ALEXNET)
         },
         'resnet18': {
             'imagenet':
                 zoo.ModelConfig(models.resnet18_seq,
                                 pretrained=True,
-                                load_weights=False),
+                                load_weights=False,
+                                layers=CONV_LAYER_RESNET18),
             'places':
                 zoo.ModelConfig(
                     models.resnet18_seq,
                     url=f'{HOST_URL}/resnet18/places/iter_131072_weights.pth',
-                    transform_weights=lambda weights: weights['state_dict']),
+                    transform_weights=lambda weights: weights['state_dict'],
+                    layers=CONV_LAYER_RESNET18),
         },
         'vgg16': {
             'imagenet':
                 zoo.ModelConfig(models.vgg16_seq,
                                 pretrained=True,
-                                load_weights=False),
+                                load_weights=False,
+                                layers=CONV_LAYERS_VGG16),
             'places':
                 zoo.ModelConfig(
                     models.vgg16_seq,
                     url=f'{HOST_URL}/vgg16/places/iter_131072_weights.pth',
-                    transform_weights=lambda weights: weights['state_dict']),
+                    transform_weights=lambda weights: weights['state_dict'],
+                    layers=CONV_LAYERS_VGG16),
         },
     }
 
