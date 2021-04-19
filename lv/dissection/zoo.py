e6bf2d3c278082273fb66ecaabcbc3b82dd90473
Evan Hernandez <evanfredhernandez@gmail.com>
2021-04-19 18:47:11 -0400

    Fix dataset keys.

--- lv/dissection/zoo.py
+++ lv/dissection/zoo.py
@@ -9,7 +9,15 @@ from third_party.netdissect import renormalize
 from torch.utils import data
 from torchvision import datasets, transforms
 
-HOST_URL = 'http://wednesday.csail.mit.edu/dez/vocabulary/models'
+MODEL_HOST = 'http://wednesday.csail.mit.edu/dez/vocabulary/models'
+MODEL_FILE_PLACES = 'iter_131072_weights.pth'
+
+KEY_ALEXNET = 'alexnet'
+KEY_RESNET18 = 'resnet18'
+KEY_VGG_16 = 'vgg16'
+
+KEY_IMAGENET = 'imagenet'
+KEY_PLACES365 = 'places365'
 
 CONV_LAYERS_ALEXNET = ('conv1', 'conv2', 'conv3', 'conv4', 'conv5')
 CONV_LAYER_RESNET18 = ('conv1', 'layer1', 'layer2', 'layer3', 'layer4')
@@ -21,42 +29,42 @@ CONV_LAYERS_VGG16 = ('conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1',
 def dissection_models() -> zoo.ModelConfigs:
     """Return configs for all models used in dissection."""
     return {
-        'alexnet': {
-            'imagenet':
+        KEY_ALEXNET: {
+            KEY_IMAGENET:
                 zoo.ModelConfig(models.alexnet_seq,
                                 pretrained=True,
                                 load_weights=False,
                                 layers=CONV_LAYERS_ALEXNET),
-            'places':
+            KEY_PLACES365:
                 zoo.ModelConfig(
                     alexnet.AlexNet,
-                    url=f'{HOST_URL}/alexnet/places/iter_131072_weights.pth',
+                    url=f'{MODEL_HOST}/alexnet/places/{MODEL_FILE_PLACES}',
                     transform_weights=lambda weights: weights['state_dict'],
                     layers=CONV_LAYERS_ALEXNET)
         },
-        'resnet18': {
-            'imagenet':
+        KEY_RESNET18: {
+            KEY_IMAGENET:
                 zoo.ModelConfig(models.resnet18_seq,
                                 pretrained=True,
                                 load_weights=False,
                                 layers=CONV_LAYER_RESNET18),
-            'places':
+            KEY_PLACES365:
                 zoo.ModelConfig(
                     models.resnet18_seq,
-                    url=f'{HOST_URL}/resnet18/places/iter_131072_weights.pth',
+                    url=f'{MODEL_HOST}/resnet18/places/{MODEL_FILE_PLACES}',
                     transform_weights=lambda weights: weights['state_dict'],
                     layers=CONV_LAYER_RESNET18),
         },
-        'vgg16': {
-            'imagenet':
+        KEY_VGG_16: {
+            KEY_IMAGENET:
                 zoo.ModelConfig(models.vgg16_seq,
                                 pretrained=True,
                                 load_weights=False,
                                 layers=CONV_LAYERS_VGG16),
-            'places':
+            KEY_PLACES365:
                 zoo.ModelConfig(
                     models.vgg16_seq,
-                    url=f'{HOST_URL}/vgg16/places/iter_131072_weights.pth',
+                    url=f'{MODEL_HOST}/vgg16/places/{MODEL_FILE_PLACES}',
                     transform_weights=lambda weights: weights['state_dict'],
                     layers=CONV_LAYERS_VGG16),
         },
@@ -67,7 +75,7 @@ def dissection_datasets() -> zoo.DatasetConfigs:
     """Return configs for all datasets used in dissection."""
     # TODO(evandez): Are these the right transforms?
     return {
-        'imagenet':
+        KEY_IMAGENET:
             zoo.DatasetConfig(datasets.ImageNet,
                               split='val',
                               transform=transforms.Compose([
@@ -76,7 +84,7 @@ def dissection_datasets() -> zoo.DatasetConfigs:
                                   transforms.ToTensor(),
                                   renormalize.NORMALIZER['imagenet']
                               ])),
-        'places365':
+        KEY_PLACES365:
             zoo.DatasetConfig(datasets.Places365,
                               split='val',
                               download=True,
