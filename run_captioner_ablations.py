cb36780d4f27b6e7269313d3913f5f8aa19f6c78
Evan Hernandez <evanfredhernandez@gmail.com>
2021-08-20 19:40:27 -0400

    Add missing beam_size argument...

--- run_captioner_ablations.py
+++ run_captioner_ablations.py
@@ -230,4 +230,7 @@ for config in args.encoders:
                 for temperature in numpy.arange(args.mi_temperature_min,
                                                 args.mi_temperature_max,
                                                 args.mi_temperature_step):
-                    evaluate(strategy='beam', mi=True, temperature=temperature)
+                    evaluate(strategy='beam',
+                             beam_size=beam_size,
+                             mi=True,
+                             temperature=temperature)
