7ecd06e23395de39da6a4c65c2f3b943b3692e28
Evan Hernandez <evanfredhernandez@gmail.com>
2021-11-30 01:20:54 -0500

    Finish export script.

--- run_export.py
+++ run_export.py
@@ -1,12 +1,15 @@
 """Export top images and descriptions to a zip file with json metadata."""
 import argparse
+import json
 import pathlib
 import shutil
+from typing import Dict
 
 from lv import datasets, models, zoo
 from lv.utils import env
 
 from torch import cuda
+from tqdm.auto import tqdm
 
 SOURCES = (
     zoo.KEYS.DENSENET121_IMAGENET,
@@ -48,6 +51,12 @@ parser.add_argument('--captioner',
                     nargs=2,
                     default=(zoo.KEYS.CAPTIONER_RESNET101, zoo.KEYS.ALL),
                     help='captioner to use (default: captioner-resnet101 all)')
+parser.add_argument('--base-url',
+                    default='https://unitname.csail.mit.edu/catalog',
+                    help='base url for images (default: csail url)')
+parser.add_argument('--no-save-images',
+                    action='store_true',
+                    help='do not save top images')
 parser.add_argument('--device', help='manually set device (default: guessed)')
 args = parser.parse_args()
 
@@ -69,11 +78,13 @@ decoder.to(device)
 assert isinstance(decoder, models.Decoder)
 
 # Read in all datasets.
-data = {}
+data: Dict[str, datasets.TopImagesDataset] = {}
 for key in args.sources:
-    data[key] = zoo.dataset(key,
-                            factory=datasets.TopImagesDataset,
-                            path=data_dir / key)
+    dataset = zoo.dataset(key,
+                          factory=datasets.TopImagesDataset,
+                          path=data_dir / key)
+    assert isinstance(dataset, datasets.TopImagesDataset)
+    data[key] = dataset
 
 # Caption all the data.
 captions = {}
@@ -93,3 +104,47 @@ for key in args.sources:
         captions_file.parent.mkdir(exist_ok=True, parents=True)
         with captions_file.open('w') as handle:
             handle.write('\n'.join(predictions))
+
+# Save images and JSON files.
+json_dir = results_dir / 'json'
+images_dir = results_dir / 'images'
+for key, dataset in data.items():
+    # TODO(evandez): Make this less hacky.
+    name = key.replace('/', '_')
+    arch = key.split('/')[0]
+    exported = {
+        'name': name,
+        'architecture': arch,
+        'dataset': key[len(arch) + 1:],
+        'layers': dataset.layers,
+    }
+    exported['units'] = units = []
+
+    model_images_dir = images_dir / name
+    if not args.no_save_images:
+        model_images_dir.mkdir(exist_ok=True, parents=True)
+    for index in tqdm(range(len(dataset)), desc=f'save {key} images'):
+        sample = dataset[index]
+        layer, unit = sample.layer, sample.unit
+
+        images = sample.as_pil_images()
+        image_files = []
+        for position, image in enumerate(images):
+            image_file = model_images_dir / f'{layer}_{unit}_{position}.png'
+            if not args.no_save_images:
+                image.save(image_file)
+            image_files.append(image_file)
+        units.append({
+            'layer': layer,
+            'unit': unit,
+            'images': [
+                f'{args.base_url}/{image_file.name}'
+                for image_file in image_files
+            ],
+            'description': captions[key][index],
+        })
+
+    model_json_file = json_dir / name / 'data.json'
+    model_json_file.parent.mkdir(exist_ok=True, parents=True)
+    with model_json_file.open('w') as handle:
+        json.dump(data, handle)
