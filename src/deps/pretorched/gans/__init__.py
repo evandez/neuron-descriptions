fc937e078bf3358eb4cb3c63ae0ab8bcf663df49
Evan Hernandez <evanfredhernandez@gmail.com>
2022-02-21 16:20:14 -0500

    Disable stylegan/stylegan2, just slows us down.

--- src/deps/pretorched/gans/__init__.py
+++ src/deps/pretorched/gans/__init__.py
@@ -3,16 +3,18 @@ from . import sagan
 from . import sngan
 from . import utils
 from .biggan import BigGAN
-from .stylegan import stylegan
+# from .stylegan import stylegan
 from .proggan import proggan
 from .biggan_deep import BigGANDeep
 
-try:
-    from .stylegan2 import stylegan2
-except EnvironmentError:
-    stylegan2 = None
-    print('Warning: could not compile cuda code for stylegan2')
-    print('Ensure $CUDA_HOME/bin/nvcc exists!')
+# try:
+#     from .stylegan2 import stylegan2
+# except EnvironmentError:
+#     stylegan2 = None
+#     print('Warning: could not compile cuda code for stylegan2')
+#     print('Ensure $CUDA_HOME/bin/nvcc exists!')
+stylegan = None
+stylegan2 = None
 
 __all__ = [
     'BigGAN',
