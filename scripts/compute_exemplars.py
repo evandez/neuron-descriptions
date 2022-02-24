1dd396873853517234257ad17346f4c40806dca9
Evan Hernandez <evanfredhernandez@gmail.com>
2022-02-24 18:00:39 -0500

    Link compute_exemplars output to data_dir.

--- scripts/compute_exemplars.py
+++ scripts/compute_exemplars.py
@@ -7,29 +7,34 @@ from src.utils import env
 
 from torch import cuda
 
-parser = argparse.ArgumentParser(description='dissect a vision model')
+parser = argparse.ArgumentParser(description='compute unit exemplars')
 parser.add_argument('model', help='model architecture')
 parser.add_argument('dataset', help='dataset model is trained on')
 parser_ex = parser.add_mutually_exclusive_group()
 parser_ex.add_argument('--layer-names',
                        nargs='+',
-                       help='layer names to dissect')
-parser_ex.add_argument(
-    '--layer-indices',
+                       help='layer names to compute exemplars for')
+parser_ex.add_argument('--layer-indices',
+                       type=int,
+                       nargs='+',
+                       help='layer indices to compute exemplars for; '
+                       'cannot be used with --layers')
+parser.add_argument(
+    '--units',
     type=int,
-    nargs='+',
-    help='layer indices to dissect; cannot be used with --layers')
-parser.add_argument('--units',
-                    type=int,
-                    help='only dissect the first n units (default: all)')
+    help='only compute exemplars for first n units (default: all)')
+parser.add_argument('--data-root',
+                    type=pathlib.Path,
+                    help='link results (in --results-root) to this directory '
+                    '(default: <project data dir> / model / dataset)')
 parser.add_argument('--results-root',
                     type=pathlib.Path,
-                    help='dissection results root '
-                    '(default: <project results dir> / dissection)')
+                    help='exemplars results root '
+                    '(default: <project results dir> / exemplars)')
 parser.add_argument('--viz-root',
                     type=pathlib.Path,
-                    help='dissection visualization root '
-                    '(default: <project results dir> / dissection / viz)')
+                    help='exemplars visualization root '
+                    '(default: <project results dir> / exemplars / viz)')
 parser.add_argument('--model-file',
                     type=pathlib.Path,
                     help='path to model weights')
@@ -39,6 +44,9 @@ parser.add_argument('--dataset-path',
 parser.add_argument('--no-viz',
                     action='store_true',
                     help='do not compute visualization')
+parser.add_argument('--no-link',
+                    action='store_true',
+                    help='do not link results to data dir')
 parser.add_argument('--num-workers',
                     type=int,
                     default=16,
@@ -72,9 +80,14 @@ units = None
 if args.units:
     units = range(args.units)
 
+data_root = args.data_root
+if data_root is None:
+    data_root = env.data_dir()
+data_dir = data_root / args.model / args.dataset
+
 results_root = args.results_root
 if results_root is None:
-    results_root = env.results_dir() / 'dissection'
+    results_root = env.results_dir() / 'exemplars'
 results_dir = results_root / args.model / args.dataset
 
 viz_root = args.viz_root
@@ -107,3 +120,6 @@ for layer in layers:
                                device=device,
                                num_workers=args.num_workers,
                                **config.exemplars.kwargs)
+
+if not args.no_link:
+    results_dir.symlink_to(data_dir, target_is_directory=True)
